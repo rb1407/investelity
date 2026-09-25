1b85a97d039950c16b0982f5d4b76775c5c6d8da
rb1407 <rishabhbasu15@gmail.com>
2026-09-25 18:25:52 +0530

    Update clean_prices.py

--- clean_prices.py
+++ clean_prices.py
@@ -11,14 +11,13 @@ Drop assets with any out-of-range prices
 (Range = [Q1 - 1.5IQR, Q3 + 1.5IQR])
 """
 def drop_outliers(prices):
-    for i in prices.columns:
-        q1 = prices[i].quantile(0.25)
-        q3 = prices[i].quantile(0.75)
-        iqr = q3 - q1
-        if (prices[i] < q1 - 1.5*iqr).any() | (prices[i] > q3 + 1.5*iqr).any() == True:
-            prices = prices.drop(i, axis = 1)
-            
-    return prices
+    q1 = prices.quantile(0.25)
+    q3 = prices.quantile(0.75)
+    iqr = q3 - q1
+    lower = q1 - 1.5 * iqr
+    upper = q3 + 1.5 * iqr
+    has_outlier = (prices.lt(lower) | prices.gt(upper)).any()
+    return prices.loc[:, ~has_outlier]
 
 indices = pd.read_excel("Markets_by_Country.xlsx", index_col = 0, header = 0)
 looper = list(indices.index)
